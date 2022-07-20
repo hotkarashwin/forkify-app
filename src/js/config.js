698dc004cf5daee738847d62e4619635b847d53c
Ashwin <hotkarashwin@gmail.com>
2022-07-20 16:39:02 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bfc327e9-110d-407f-bba1-b0e5f1686007';
+export const MODAL_CLOSE_SEC = 2.5;
